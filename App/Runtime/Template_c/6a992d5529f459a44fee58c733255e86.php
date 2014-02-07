e44f6c60500f1cb1144fa069c7ee0c6eaa886290
nly <nly92@foxmail.com>
2014-02-07 18:04:46 +0800

    添加load引入js、css的示例

--- App/Runtime/Template_c/6a992d5529f459a44fee58c733255e86.php
+++ App/Runtime/Template_c/6a992d5529f459a44fee58c733255e86.php
@@ -1,3 +1,5 @@
+<script type="text/javascript" src="http://cdn.staticfile.org/jquery/2.1.0/jquery.min.js?t=1391767420"></script>
+<link type="text/css" rel="stylesheet" href="http://cdn.staticfile.org/meyer-reset/2.0/reset.min.css?t=1391767420" />
 <p>这是一个模板文件</p>
 
 <p><?php echo $this->value['name']; ?></p>
